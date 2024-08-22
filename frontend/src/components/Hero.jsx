09d066bfe7aa698921fed1a41b084539b9ffc6f3
cperalt <145234956+cperalt@users.noreply.github.com>
2024-08-22 14:22:00 -0400

    Fixed LInk

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -7,11 +7,7 @@ const HeroSection = () => {
     <section className="section center-align">
       <img src={Logo} alt="lrnr logo" className="responsive-img" style={{ maxWidth: '27%', marginTop: '3.5rem' }} />
       <h5>Your guided path to programming enlightenment</h5>
-<<<<<<< HEAD
-      <NavLink to="/quiz-generator" className="btn-large teal">Begin Journey</NavLink>
-=======
-      <a href="/quiz-generator" className="btn-large teal" style={{marginTop: '2.5rem'}}>Begin Journey</a>
->>>>>>> 4915c6e38180e56f0ea3c1929e2ebcc3d9fdbf3f
+      <NavLink to="/quiz-generator" className="btn-large teal" style={{marginTop: '2.5rem'}}>Begin Journey</NavLink>
     </section>
   );
 };
